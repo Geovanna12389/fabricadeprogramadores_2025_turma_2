d4e836b0e3552589502e6c6d9aee62955a550e97
Geovanna12389 <geovannapaulademaria@gmail.com>
2025-08-25 11:49:00 -0300

    Aula de dicionário

---
+++ dicionários.py
@@ -0,0 +1,12 @@
+tabela = { "alface": 0.45,
+            "batata": 1.20,
+            "tomate": 2.30,
+            "feijão": 1.50}
+print(tabela["alface"])
+print(tabela["tomate"])
+print(tabela["batata"])
+print(tabela["feijão"])
+tabela["cebola"] = 1.20
+print(tabela)
+print("morango" in tabela)
+print("alface" in tabela)
